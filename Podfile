8b5da5aa0611170fe4465642e60e252021aa0dc6
John Kodumal <john@launchdarkly.com>
2015-10-21 16:38:35 -0700

    Initial commit

---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+source 'https://github.com/launchdarkly/private-pods.git'
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'hello-ios' do
+ pod 'LaunchDarkly'
+end
+
--- hello-ios/ViewController.m
+++ hello-ios/ViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "LDClient.h"
+#import "LDConfig.h"
+#import "LDUserBuilder.h"
+
 
 @interface ViewController ()
 
@@ -17,6 +21,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    LDUserBuilder *builder = [[LDUserBuilder alloc] init];
+    builder = [builder withKey:@"bob@example.com"];
+    builder = [builder withFirstName:@"Bob"];
+    builder = [builder withLastName:@"Loblaw"];
+    
+    NSArray *groups = @[@"beta_testers"];
+    builder = [builder withCustomArray:@"groups" value:groups];
+    
+    User *user = [builder build];
+
+    LDConfigBuilder *config = [[LDConfigBuilder alloc] init];
+    [config withApiKey:@"YOUR_MOBILE_KEY"];
+    
+    [[LDClient sharedInstance] start:[config build] user:user];
+    
+    BOOL showFeature = [[LDClient sharedInstance] toggle:@"YOUR_FLAG_KEY" default:NO];
+    if (showFeature) {
+        NSLog(@"Showing feature for %@", user.key);
+    } else {
+        NSLog(@"Not showing feature for %@", user.key);
+    }
+
+
 }
 
 - (void)didReceiveMemoryWarning {
